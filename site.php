01d87ee7471a26e1bbd7145f9748483eb1289d8e
Kha Truong <khatruong2009@gmail.com>
2020-09-01 18:35:15 -0400

    initial commit/setup

---
+++ site.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tip Calculator</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  
+  <div class="header">
+    <h1>Tip Calculator</h1>
+  </div>
+
+  <form action="site.php" method="post">
+    How much was the bill total?
+    <br>
+    $<input type="number" name="bill"/>
+  </form>
+
+  <?php 
+
+  ?>
+
+</body>
+</html>
